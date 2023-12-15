85416b9a41ba54cdca9f072cb4f8f8469a8b9209
GerardCaja <120013160+GerardCaja@users.noreply.github.com>
2023-12-15 12:36:53 +0100

    dsfsf

--- Survivor-main/Assets/Scripts/MenuPause.cs
+++ Survivor-main/Assets/Scripts/MenuPause.cs
@@ -8,33 +8,12 @@ public class MenuPause : MonoBehaviour
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
     [SerializeField] private GameObject options;
-    [SerializeField] private GameObject menuWin;
-    [SerializeField] private GameObject menuDerrota;
+
     private Animator _animator;
 
     void Start()
     {
         _animator = GetComponentInChildren<Animator>();
-    }
-
-    public void MenuWin()
-    {
-        
-    }
-
-    public void NextLevel()
-    {
-
-    }
-
-    public void MenuDerrota()
-    {
-
-    }
-
-    public void Reintentar()
-    {
-        
     }
 
     public void Pausa()
@@ -43,6 +22,7 @@ public class MenuPause : MonoBehaviour
         botonPausa.SetActive(false);
         menuPausa.SetActive(true);
         options.SetActive(false);
+        
 
         _animator.SetBool("MenuPause", true);
     }
